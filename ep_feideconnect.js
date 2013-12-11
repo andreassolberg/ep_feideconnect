6e8b37a314dc2e9ca7d3bb9de464f7fc78bd95b4
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2013-12-11 08:04:45 +0100

    Fix

--- ep_feideconnect.js
+++ ep_feideconnect.js
@@ -1,18 +1,53 @@
-var jso = require('jso');
+var 
+	express = require('express'),
+	jso = require('jso')
+	;
 
-var settings = require('ep_etherpad-lite/node/utils/Settings');
-var exp = require('ep_etherpad-lite/node_modules/express');
+var settings = require('/Users/andreas/wc/etherpad-lite/src/node/utils/Settings');
+// var settings = require('../../src/node/utils/Settings');
 
 console.log("Loaded jso", jso);
 
+var sessionConfig = { 
+	// path: '/', 
+	// httpOnly: true, 
+	// maxAge: null,
+	"secret": "slksdf89sd8ftsdjhfgsduytf",
+	"store": store,
+	"cookie": { 
+		path: '/', 
+		httpOnly: true, 
+		maxAge: 1000*24*60*60*1000 // , // 10000 days.
+		// secure: true
+	}
+};
 exports.expressConfigure = function(hook_name, args, cb) {
 
-	console.log("EXPRESS PLUGIN FEIDE CONNECT Talking.. Whos there?", hook_name, args);
+	console.log(" >>>> EXPRESS PLUGIN FEIDE CONNECT Talking.. Whos there?");
+	var options = settings.ep_feideconnect;
+	console.log(options);
+
+	var fc = new jso.FeideConnect(options.oauth);
 
 	args.app.use(function(req, res, next) {
 		if (req.path.match(/^\/(static|javascripts|pluginfw)/)) {
 			next();
 		} else {
+
+			var app = args.app;
+
+			app.use(express.cookieParser());
+			app.use(express.session(sessionConfig));
+
+			app.use('/callback', fc.getAuthenticationMiddleware().callback().authenticate() );
+
+			// app.use('/', o.getAuthenticationMiddleware() );
+			app.use('/test', fc.getAuthenticationMiddleware()
+				.requireScopes(['userinfo'])
+
+			);
+			app.use('/', fc.getAuthenticationMiddleware());
+			app.use(app.router);
 
 			next();
 
