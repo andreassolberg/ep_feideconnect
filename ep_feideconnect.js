5f25baabf88381a2082e2e84d5a94f82c6786a17
Andreas Åkre Solberg <andreas.uninett@uninett.no>
2013-12-12 12:52:13 +0100

    Improvements.

--- ep_feideconnect.js
+++ ep_feideconnect.js
@@ -3,11 +3,11 @@ var
 	jso = require('jso')
 	;
 
-var settings = require('/Users/andreas/wc/etherpad-lite/src/node/utils/Settings');
+var settings = require('/Users/andreas/wcn/etherpad-lite/src/node/utils/Settings');
 // var settings = require('../../src/node/utils/Settings');
 
 console.log("Loaded jso", jso);
-
+var store = new express.session.MemoryStore();
 var sessionConfig = { 
 	// path: '/', 
 	// httpOnly: true, 
@@ -23,39 +23,59 @@ var sessionConfig = {
 };
 exports.expressConfigure = function(hook_name, args, cb) {
 
-	console.log(" >>>> EXPRESS PLUGIN FEIDE CONNECT Talking.. Whos there?");
 	var options = settings.ep_feideconnect;
-	console.log(options);
-
 	var fc = new jso.FeideConnect(options.oauth);
+	var app = args.app;
 
-	args.app.use(function(req, res, next) {
-		if (req.path.match(/^\/(static|javascripts|pluginfw)/)) {
-			next();
-		} else {
+	console.log(" >>>> EXPRESS PLUGIN FEIDE CONNECT Talking.. Whos there?");
+	console.log(options);	
 
-			var app = args.app;
+	app.use(express.cookieParser());
+	app.use(express.session(sessionConfig));
 
-			app.use(express.cookieParser());
-			app.use(express.session(sessionConfig));
+	app.use('/callback/FeideConnect', 
+		fc.getMiddleware()
+			.callback()
+			.authenticate() 
+	);
 
-			app.use('/callback', fc.getAuthenticationMiddleware().callback().authenticate() );
+	// app.use('/', o.getAuthenticationMiddleware() );
+	app.use('/p/', 
+		fc.getMiddleware()
+			.requireScopes(['userinfo'])
+	);
+	// app.use('/', fc.getMiddleware());
 
-			// app.use('/', o.getAuthenticationMiddleware() );
-			app.use('/test', fc.getAuthenticationMiddleware()
-				.requireScopes(['userinfo'])
+	app.use('/p/', function(req, res, next) {
 
-			);
-			app.use('/', fc.getAuthenticationMiddleware());
-			app.use(app.router);
+		console.log("About to present session");
+		console.log(req.session);
 
-			next();
-
-			// github.orgAccess({
-			// 	appId: settings.users.github.appId,
-			// 	appSecret: settings.users.github.appSecret,
-			// 	callback: settings.users.github.callback
-			// }, settings.users.github.org).handle(req, res, next);
-		}
+		next();
 	});
+
+
+	app.use(app.router);
+
+
+
+
+
+
+	// args.app.use(function(req, res, next) {
+	// 	if (req.path.match(/^\/(static|javascripts|pluginfw)/)) {
+	// 		next();
+	// 	} else {
+
+			
+
+
+
+	// 		// github.orgAccess({
+	// 		// 	appId: settings.users.github.appId,
+	// 		// 	appSecret: settings.users.github.appSecret,
+	// 		// 	callback: settings.users.github.callback
+	// 		// }, settings.users.github.org).handle(req, res, next);
+	// 	}
+	// });
 }
